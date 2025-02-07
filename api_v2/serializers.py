f6ca267b14329cab9a83b80524e1ce0d42123df4
iamaswathisnair <snairaswathi5@gmail.com>
2025-02-07 21:27:16 +0530

    dept and SerializerMethodField()

--- api_v2/serializers.py
+++ api_v2/serializers.py
@@ -20,7 +20,19 @@ class TeacherSerializer(serializers.ModelSerializer):
         read_only_fields =['name' , 'age']
         
         
-#custome validation in creete()        
+#custome validation in creete()  
+      
 # def create(self, validated_data):
 #         validated_data['password'] = hash_function(validated_data['password'])  # Hash password
 #         return super().create(validated_data)
+
+
+# def create(self, validated_data):
+#         if 'department' not in validated_data:
+#             validated_data['department'] = Department.objects.get(name="General")
+#         return super().create(validated_data)  
+           
+# def update(self, instance, validated_data):
+#         if 'name' in validated_data:
+#             raise serializers.ValidationError("You cannot update a teacher's name!")
+#         return super().update(instance, validated_data)
